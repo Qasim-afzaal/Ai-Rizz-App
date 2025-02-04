270976f5dc698778b54d3e23afc45989392e9cba
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-02-05 04:54:41 +0500

    add imports

---
+++ lib/core/constants/imports.dart
@@ -0,0 +1,32 @@
+export 'package:flutter/material.dart';
+
+export 'package:sparkd/core/constants/app_strings.dart';
+export 'package:sparkd/core/components/sb.dart';
+export 'package:sparkd/gen/assets.gen.dart';
+
+export 'package:sparkd/core/components/app_button.dart';
+export 'package:sparkd/core/components/app_bar.dart';
+export 'package:sparkd/core/components/app_text_field.dart';
+
+///Enums
+export 'package:sparkd/core/enums/genders.dart';
+export 'package:sparkd/core/enums/personality.dart';
+
+export 'package:sparkd/core/enums/chat_type.dart';
+export 'package:sparkd/models/message_model.dart';
+
+/// Pages=
+
+/// Widgets=
+
+/// Extensions
+export 'package:sparkd/core/extensions/build_context_extension.dart';
+
+/// PACKAGE
+export 'package:get/get.dart'
+    hide ContextExtensionss, FormData, MultipartFile, Response;
+export 'package:flutter_svg/flutter_svg.dart';
+
+export 'package:sparkd/core/constants/constants.dart';
+export 'package:sparkd/pages/dashboard/dashboard.dart';
+
